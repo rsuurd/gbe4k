35fcc90488a2f978ff0ce1414f6b9aed85557e4a
rolf <rolf.suurd@gmail.com>
2025-02-22 12:56:07 +0100

    💿 added Cart

---
+++ src/main/kotlin/gbe4k/core/Cart.kt
@@ -0,0 +1,21 @@
+package gbe4k.core
+
+import java.nio.file.Files
+import java.nio.file.Path
+
+class Cart(private val data: ByteArray) {
+    val title: String get() = String(data.sliceArray(IntRange(0x134, 0x142))).replace("\u0000", "")
+    val size: Int get() = 32 shl data[0x148].toInt()
+
+    fun read(address: Int) = data[address].toInt()
+
+    override fun toString() = "Cart(title='$title', size=$size kb)"
+
+    companion object {
+        fun load(path: Path): Cart {
+            val bytes = Files.readAllBytes(path)
+
+            return Cart(bytes)
+        }
+    }
+}
---
+++ src/test/kotlin/gbe4k/core/CartTest.kt
@@ -0,0 +1,41 @@
+package gbe4k.core
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.net.URI
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.zip.ZipInputStream
+import kotlin.io.path.exists
+
+class CartTest {
+    @BeforeEach
+    fun `download test roms`() {
+        with(Paths.get(".roms")) {
+            if (!exists() || toFile().listFiles()?.isEmpty() == true) {
+                ZipInputStream(
+                    URI("https://github.com/retrio/gb-test-roms/archive/refs/heads/master.zip").toURL().openStream()
+                ).use { zip ->
+                    generateSequence { zip.nextEntry }.forEach { entry ->
+                        val filename = entry.name.removePrefix("gb-test-roms-master/")
+
+                        if (entry.isDirectory) {
+                            Files.createDirectories(resolve(filename))
+                        } else {
+                            Files.write(resolve(filename), zip.readAllBytes())
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `should load cart`() {
+        val cart = Cart.load(Paths.get(".roms/cpu_instrs/cpu_instrs.gb"))
+
+        assertThat(cart.title).isEqualTo("CPU_INSTRS")
+        assertThat(cart.size).isEqualTo(64)
+    }
+}
