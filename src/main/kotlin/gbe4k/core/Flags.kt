ddc673fd7cbab07f1dd17fa783961a140e8d9adb
rolf <rolf.suurd@gmail.com>
2025-02-26 17:59:17 +0100

    💩 flags use shared bit manipulation code

--- src/main/kotlin/gbe4k/core/Flags.kt
+++ src/main/kotlin/gbe4k/core/Flags.kt
@@ -1,51 +1,30 @@
 package gbe4k.core
 
-import kotlin.experimental.and
-import kotlin.experimental.or
+import gbe4k.core.Cpu.Companion.isBitSet
+import gbe4k.core.Cpu.Companion.setBit
 
 class Flags(private val registers: Registers) {
     var z: Boolean
-        get() = getFlag(0)
+        get() = registers.f.isBitSet(0)
         set(value) {
-
-            registers.f = setFlag(value, 0)
+            registers.f = registers.f.setBit(value, 0)
         }
 
     var n: Boolean
-        get() = getFlag(1)
+        get() = registers.f.isBitSet(1)
         set(value) {
-            registers.f = setFlag(value, 1)
+            registers.f = registers.f.setBit(value, 1)
         }
 
     var h: Boolean
-        get() = getFlag(2)
+        get() = registers.f.isBitSet(2)
         set(value) {
-            registers.f = setFlag(value, 2)
+            registers.f = registers.f.setBit(value, 2)
         }
 
     var c: Boolean
-        get() = getFlag(3)
+        get() = registers.f.isBitSet(3)
         set(value) {
-            registers.f = setFlag(value, 3)
+            registers.f = registers.f.setBit(value, 3)
         }
-
-    fun set(z: Boolean? = null, n: Boolean? = null, h: Boolean? = null, c: Boolean? = null) {
-        z?.let { this.z = it }
-        n?.let { this.n = it }
-        h?.let { this.h = it }
-        c?.let { this.c = it }
-    }
-
-    private fun getFlag(position: Int) =
-        (registers.f.toInt() shr position).and(1) == 1
-
-    private fun setFlag(enabled: Boolean, position: Int): Byte {
-        return if (enabled) {
-            registers.f or (1 shl position).toByte()
-        } else {
-            val size = Byte.SIZE_BITS - registers.f.countLeadingZeroBits()
-
-            registers.f and (((1 shl size) - 1) - (1 shl position)).toByte()
-        }
-    }
 }
